112945b15604b50d7886a2471d9b6fd006d71401
Beukens <beukensgaetan@gmail.com>
2023-12-18 10:55:21 +0100

    Add Maven wrapper and initial Spring Boot setup
    This commit introduces the Maven wrapper to ensure consistent builds across environments and kick-starts the Spring Boot setup. It sets up the main application class, basic test case and provides initial application properties file for Spring Data JPA. Additionally, PostgreSQL is chosen as the database, and Lombok library is included for reducing the boilerplate code.

---
+++ src/main/java/be/yapock/pokemonsleepcalculator/PokemonSleepCalculatorApplication.java
@@ -0,0 +1,13 @@
+package be.yapock.pokemonsleepcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonSleepCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonSleepCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/yapock/pokemonsleepcalculator/PokemonSleepCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package be.yapock.pokemonsleepcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonSleepCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
