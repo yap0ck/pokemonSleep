18f0fd707947457c0320dd20c29d2618d1a8c032
Beukens <beukensgaetan@gmail.com>
2023-12-18 15:36:29 +0100

    Add error handling for EntityNotFoundException
    A ControllerAdvisor class is introduced in this commit, which handles EntityNotFoundException using @ExceptionHandler. It responds by creating and returning an Error object that encapsulates detailed error information. This enhances the system's resilience and provides valuable debug information when such an exception

---
+++ src/main/java/be/yapock/pokemonsleepcalculator/pl/controllers/ControllerAdvisor.java
@@ -0,0 +1,27 @@
+package be.yapock.pokemonsleepcalculator.pl.controllers;
+
+import be.yapock.pokemonsleepcalculator.pl.models.error.Error;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class ControllerAdvisor {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Error> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
+        Error error = Error.builder()
+                .status(HttpStatus.NOT_FOUND)
+                .message(e.getMessage())
+                .requestMadeAt(LocalDateTime.now())
+                .URI(request.getRequestURI())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(error);
+    }
+}
---
+++ src/main/java/be/yapock/pokemonsleepcalculator/pl/models/error/Error.java
@@ -0,0 +1,16 @@
+package be.yapock.pokemonsleepcalculator.pl.models.error;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class Error {
+    private String message;
+    private HttpStatus status;
+    private LocalDateTime requestMadeAt;
+    private String URI;
+}
